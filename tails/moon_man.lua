cdfaf642efec5495aaf4697a687d4632c29937ae
Erisen <203582669+Erisen8974@users.noreply.github.com>
2025-09-19 22:37:36 -0400

    status messaging

--- tails/moon_man.lua
+++ tails/moon_man.lua
@@ -4,6 +4,9 @@ local MAX_RESEARCH = Config.Get("MaxResearch")
 
 local current_job = Player.Job
 
+log("Starting auto relic on job", current_job.Name, "(" .. current_job.Abbreviation .. ")")
+log("Gamba limit:", GAMBA_TIME, "Max research:", MAX_RESEARCH, "Handle retainers:", PROCESS_RETAINERS)
+
 if current_job.Abbreviation == "FSH" then
     fish_relic(MAX_RESEARCH)
 elseif current_job.IsGatherer then
@@ -13,3 +16,5 @@ elseif current_job.IsCrafter then
 else
     log("Invalid job", current_job.Name, "only gatherers are supported") -- update message when crafters are supported
 end
+
+log("Finished auto relic on job", current_job.Name, "(" .. current_job.Abbreviation .. ")")
